fdaee1fb90c49ef3144bf52542648f54dfeea7db
greyTurtleDove <radek29c@gmail.com>
2024-10-04 14:31:20 +0200

    Enemys movement upgrades

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public enum Direction
@@ -19,7 +20,7 @@ public class Enemy : MonoBehaviour
 
     public Direction direction;
     int WalkLength = 500;
-    [SerializeField] float velocity = 1.5f;
+    [SerializeField] float velocity = 3f;
 
     Vector2 StartOfMap = new Vector2(-8.86f, 9.46f);
     Vector2 EndOfMap = new Vector2(8.9f, -5f);
@@ -73,6 +74,38 @@ public class Enemy : MonoBehaviour
         {
             direction = (Direction)Random.Range(0, 4);
             WalkLength = 120;
+        }
+        if(Time.frameCount % 20 == 0)
+        {
+            const float MinDist = 2f;
+            const float MinDistNarrow = 1f;
+            foreach (GameObject g in GameObject.FindGameObjectsWithTag("Bullet"))
+            {
+                if (Mathf.Abs(transform.position.x - g.transform.position.x) < MinDist && Mathf.Abs(transform.position.y - g.transform.position.y) < MinDistNarrow && g.GetComponent<Rigidbody2D>().velocity.x != 0)
+                {
+                    float r = Random.value;
+                    if (r < 0.45f)
+                    {
+                        direction = Direction.Up;
+                    }
+                    else if(r < 0.9f)
+                    {
+                        direction = Direction.Down;
+                    }
+                }
+                else if (Mathf.Abs(transform.position.y - g.transform.position.y) < MinDist && Mathf.Abs(transform.position.x - g.transform.position.x) < MinDistNarrow && g.GetComponent<Rigidbody2D>().velocity.y != 0)
+                {
+                    float r = Random.value;
+                    if (r < 0.45f)
+                    {
+                        direction = Direction.Right;
+                    }
+                    else if(r < 0.9f)
+                    {
+                        direction = Direction.Left;
+                    }
+                }
+            }
         }
         Move(direction);
 
